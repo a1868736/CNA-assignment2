c1ddc2880253380bce6b5d5ddf252214d598e9ca
fafa <txdyscly@gmail.com>
2025-05-04 04:04:34 +0930

    finish A_timerinterrupt part, change the timer setting, just resend if windowfirst is not ACKed.

--- sr.c
+++ sr.c
@@ -135,18 +135,19 @@ void A_input(struct pkt packet)
 /* called when A's timer goes off */
 void A_timerinterrupt(void)
 {
-  int i;
-
   if (TRACE > 0)
     printf("----A: time out,resend packets!\n");
 
-  
     if (TRACE > 0)
-      printf ("---A: resending packet %d\n", (buffer[]).seqnum);
-
-    tolayer3(A,buffer);
+      printf ("---A: resending packet %d\n", (buffer[windowfirst]).seqnum);
+    
+    /*only resend if the leftmost (earliest) packet in the window not ACKed.*/
+    tolayer3(A,buffer[windowfirst]); 
     packets_resent++;
 
+    if (windowcount > 0) {
+      starttimer(A, RTT);
+    }
   
 }
 
